2c4d16ec439b603095a47e118bee8b9f01f759bb
Abinjith K <abinjithk520@gmail.com>
2026-01-03 23:04:07 +0530

    jwt missing changes

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -1,1 +1,25 @@
-from app.core.jwt import *from app.core.jwt import *
+from datetime import datetime, timedelta
+from jose import jwt
+from app.config import get_settings
+from passlib.context import CryptContext
+
+settings = get_settings()
+
+
+pwd_context = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto"
+)
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=settings.ACCESS_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
